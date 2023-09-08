c30ce6b25e9054989907bae63fbb92d92927c4bd
Liam O'Brien <liam.obrien@atteris.com>
2023-09-08 16:49:52 +0800

    initial react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+  <Blha></Blha>
+  <Blah></Blah>
+  );
+}
+
+export default App;
